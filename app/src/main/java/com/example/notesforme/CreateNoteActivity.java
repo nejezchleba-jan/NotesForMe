2734c86aab290f64b1f6fd7ad9cf607b59d6e425
nejezchleba-jan <Jan.Nejezchleba1@seznam.cz>
2020-08-27 12:06:42 +0200

    Basic layout done
    Updated activities:
    
    CreateNoteActivity(layout + func)
    MainActivity(layout + func)

---
+++ app/src/main/java/com/example/notesforme/CreateNoteActivity.java
@@ -0,0 +1,26 @@
+package com.example.notesforme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class CreateNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+
+        ImageView imageBack = findViewById(R.id.imageBack);
+        imageBack.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+}
